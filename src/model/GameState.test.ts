e6d586a151bdc7e68fa8e329d6a3bba50ed8a2c5
Ferose B <ferose2@gmail.com>
2019-11-28 01:49:04 -0800

    Add find winning circles logic

---
+++ src/model/GameState.test.ts
@@ -0,0 +1,167 @@
+import { GameState, Circle, CircleType } from './GameState';
+
+describe('Win condition', () => {
+    const gameState = new GameState(5, 6);
+
+    test('empty state', () => {
+        expect(gameState.findWinningCircles()).toBeNull();
+    })
+
+    test('Vertical matches', () => {
+        gameState.moves = [
+            new Circle({x: 0, y: 1, type: CircleType.red}),
+            new Circle({x: 0, y: 2, type: CircleType.red}),
+            new Circle({x: 0, y: 3, type: CircleType.red}),
+            new Circle({x: 0, y: 4, type: CircleType.red}),
+        ];
+        expect(gameState.findWinningCircles()).toEqual([
+            new Circle({x: 0, y: 1, type: CircleType.red}),
+            new Circle({x: 0, y: 2, type: CircleType.red}),
+            new Circle({x: 0, y: 3, type: CircleType.red}),
+            new Circle({x: 0, y: 4, type: CircleType.red}),
+        ]);
+    });
+
+    test('Matching at start', () => {
+        gameState.moves = [
+            new Circle({x: 0, y: 0, type: CircleType.red}),
+            new Circle({x: 0, y: 1, type: CircleType.red}),
+            new Circle({x: 0, y: 2, type: CircleType.red}),
+            new Circle({x: 0, y: 3, type: CircleType.red}),
+        ];
+        expect(gameState.findWinningCircles()).toEqual([
+            new Circle({x: 0, y: 0, type: CircleType.red}),
+            new Circle({x: 0, y: 1, type: CircleType.red}),
+            new Circle({x: 0, y: 2, type: CircleType.red}),
+            new Circle({x: 0, y: 3, type: CircleType.red}),
+        ]);
+    });
+
+    test('Matching at end', () => {
+        gameState.moves = [
+            new Circle({x: 0, y: 3, type: CircleType.red}),
+            new Circle({x: 0, y: 4, type: CircleType.red}),
+            new Circle({x: 0, y: 5, type: CircleType.red}),
+            new Circle({x: 0, y: 6, type: CircleType.red}),
+        ]
+        expect(gameState.findWinningCircles()).toEqual([
+            new Circle({x: 0, y: 3, type: CircleType.red}),
+            new Circle({x: 0, y: 4, type: CircleType.red}),
+            new Circle({x: 0, y: 5, type: CircleType.red}),
+            new Circle({x: 0, y: 6, type: CircleType.red}),
+        ]);
+    });
+
+    test('Long match', () => {
+        gameState.moves = [
+            new Circle({x: 0, y: 2, type: CircleType.red}),
+            new Circle({x: 0, y: 3, type: CircleType.red}),
+            new Circle({x: 0, y: 4, type: CircleType.red}),
+            new Circle({x: 0, y: 5, type: CircleType.red}),
+            new Circle({x: 0, y: 6, type: CircleType.red}),
+        ]
+        expect(gameState.findWinningCircles()).toEqual([
+            new Circle({x: 0, y: 2, type: CircleType.red}),
+            new Circle({x: 0, y: 3, type: CircleType.red}),
+            new Circle({x: 0, y: 4, type: CircleType.red}),
+            new Circle({x: 0, y: 5, type: CircleType.red}),
+            new Circle({x: 0, y: 6, type: CircleType.red}),
+        ]);
+    });
+
+    test('Short match', () => {
+        gameState.moves = [
+            new Circle({x: 0, y: 2, type: CircleType.red}),
+            new Circle({x: 0, y: 3, type: CircleType.red}),
+            new Circle({x: 0, y: 4, type: CircleType.red}),
+        ]
+        expect(gameState.findWinningCircles()).toBeNull();
+    });
+
+    test('Different colors', () => {
+        gameState.moves = [
+            new Circle({x: 1, y: 0, type: CircleType.red}),
+            new Circle({x: 1, y: 1, type: CircleType.red}),
+            new Circle({x: 1, y: 2, type: CircleType.blue}),
+            new Circle({x: 1, y: 3, type: CircleType.red}),
+        ]
+        expect(gameState.findWinningCircles()).toBeNull();
+    });
+
+    test('Subset match', () => {
+        gameState.moves = [
+            new Circle({x: 1, y: 0, type: CircleType.red}),
+            new Circle({x: 1, y: 1, type: CircleType.red}),
+            new Circle({x: 1, y: 2, type: CircleType.red}),
+            new Circle({x: 1, y: 3, type: CircleType.red}),
+            new Circle({x: 1, y: 4, type: CircleType.blue}),
+            new Circle({x: 1, y: 5, type: CircleType.blue}),
+        ]
+        expect(gameState.findWinningCircles()).toEqual([
+            new Circle({x: 1, y: 0, type: CircleType.red}),
+            new Circle({x: 1, y: 1, type: CircleType.red}),
+            new Circle({x: 1, y: 2, type: CircleType.red}),
+            new Circle({x: 1, y: 3, type: CircleType.red}),
+        ]);
+    });
+
+    test('Horizontal match', () => {
+        gameState.moves = [
+            new Circle({x: 1, y: 0, type: CircleType.red}),
+            new Circle({x: 2, y: 0, type: CircleType.red}),
+            new Circle({x: 3, y: 0, type: CircleType.red}),
+            new Circle({x: 4, y: 0, type: CircleType.red}),
+        ]
+        expect(gameState.findWinningCircles()).toEqual([
+            new Circle({x: 1, y: 0, type: CircleType.red}),
+            new Circle({x: 2, y: 0, type: CircleType.red}),
+            new Circle({x: 3, y: 0, type: CircleType.red}),
+            new Circle({x: 4, y: 0, type: CircleType.red}),
+        ]);
+    });
+
+    test('Top left to bottom right match for top half', () => {
+        gameState.moves = [
+            new Circle({x: 1, y: 0, type: CircleType.red}),
+            new Circle({x: 2, y: 1, type: CircleType.red}),
+            new Circle({x: 3, y: 2, type: CircleType.red}),
+            new Circle({x: 4, y: 3, type: CircleType.red}),
+        ]
+        expect(gameState.findWinningCircles()).toEqual([
+            new Circle({x: 1, y: 0, type: CircleType.red}),
+            new Circle({x: 2, y: 1, type: CircleType.red}),
+            new Circle({x: 3, y: 2, type: CircleType.red}),
+            new Circle({x: 4, y: 3, type: CircleType.red}),
+        ]);
+    });
+
+    test('Top left to bottom right match for bottom half', () => {
+        gameState.moves = [
+            new Circle({x: 0, y: 1, type: CircleType.red}),
+            new Circle({x: 1, y: 2, type: CircleType.red}),
+            new Circle({x: 2, y: 3, type: CircleType.red}),
+            new Circle({x: 3, y: 4, type: CircleType.red}),
+        ]
+        expect(gameState.findWinningCircles()).toEqual([
+            new Circle({x: 0, y: 1, type: CircleType.red}),
+            new Circle({x: 1, y: 2, type: CircleType.red}),
+            new Circle({x: 2, y: 3, type: CircleType.red}),
+            new Circle({x: 3, y: 4, type: CircleType.red}),
+        ]);
+    });
+
+    test('Test bottom left to top right', () => {
+        gameState.moves = [
+            new Circle({x: 3, y: 1, type: CircleType.red}),
+            new Circle({x: 2, y: 2, type: CircleType.red}),
+            new Circle({x: 1, y: 3, type: CircleType.red}),
+            new Circle({x: 0, y: 4, type: CircleType.red}),
+        ]
+        expect(gameState.findWinningCircles()).toEqual([
+            new Circle({x: 0, y: 4, type: CircleType.red}),
+            new Circle({x: 1, y: 3, type: CircleType.red}),
+            new Circle({x: 2, y: 2, type: CircleType.red}),
+            new Circle({x: 3, y: 1, type: CircleType.red}),
+        ]);
+    });
+});
--- src/model/GameState.ts
+++ src/model/GameState.ts
@@ -86,4 +86,134 @@ export class GameState {
     public get isAnimating() {
         return this.lastMove && this.lastMove.alpha === 0;
     }
+
+    /**
+     * Returns a grid with [x][y] coordinates, x being col and y being row
+     */
+    public constructGrid() {
+        const grid: (Circle|null)[][] = [];
+        for (let x = 0; x < this.numCols; x++) {
+            grid[x] = [];
+            for (let y = 0; y < this.numRows; y++) {
+                grid[x][y] = null;
+            }
+        }
+        for (const circle of this.moves) {
+            grid[circle.x][circle.y] = circle;
+        }
+        return grid;
+    }
+
+    public findWinningCircles(): Circle[] | null {
+        const grid = this.constructGrid();
+
+        const numMatches = 4;
+
+        // Vertical
+        for (let x = 0; x < grid.length; x++) {
+            let start = 0;
+            for (let end = 1; end < grid[x].length; end++) {
+                const startCircle = grid[x][start];
+                const endCircle = grid[x][end];
+                if (startCircle === null ||
+                    endCircle === null ||
+                    startCircle.type !== endCircle.type) {
+                    if (end - start >= numMatches) {
+                        return grid[x].slice(start, end) as Circle[];
+                    }
+                    start = end;
+                }
+            }
+            if (grid[x].length - start >= numMatches) {
+                return grid[x].slice(start, grid[x].length) as Circle[];
+            }
+        }
+
+        // Horizontal
+        for (let y = 0; y < grid[0].length; y++) {
+            let start = 0;
+            for (let end = 1; end < grid.length; end++) {
+                const startCircle = grid[start][y];
+                const endCircle = grid[end][y];
+                if (startCircle === null ||
+                    endCircle === null ||
+                    startCircle.type !== endCircle.type) {
+                    if (end - start >= numMatches) {
+                        return grid.map((circles) => circles[y]).slice(start, end) as Circle[];
+                    }
+                    start = end;
+                }
+            }
+            if (grid.length - start >= numMatches) {
+                return grid.map((circles) => circles[y]).slice(start, grid.length) as Circle[];
+            }
+        }
+
+        // Top left to bottom right
+        for (let x = -grid[0].length; x < grid.length; x++) {
+            let start = 0;
+            if (x < 0) {
+                start = -x;
+            }
+            let end = start+1;
+            for (; end < grid[0].length && x+end < grid.length; end++) {
+                const startCircle = grid[x+start][start];
+                const endCircle = grid[x+end][end];
+                if (startCircle === null ||
+                    endCircle === null ||
+                    startCircle.type !== endCircle.type) {
+                    if (end - start >= numMatches) {
+                        const result = [];
+                        for (let i = start; i < end; i++) {
+                            result.push(grid[x+i][i]);
+                        }
+                        return result as Circle[];
+                    }
+                    start = end;
+                }
+            }
+            if (end - start >= numMatches) {
+                const result = [];
+                for (let i = start; i < end; i++) {
+                    result.push(grid[x+i][i]);
+                }
+                return result as Circle[];
+            }
+        }
+
+
+        // Bottom left to top right
+        for (let x = -grid[0].length; x < grid.length; x++) {
+            let start = 0;
+            if (x < 0) {
+                start = -x;
+            }
+            let end = start+1;
+            for (; end < grid[0].length && x+end < grid.length; end++) {
+                const startCircle = grid[x+start][grid[0].length-1-start];
+                const endCircle = grid[x+end][grid[0].length-1-end];
+                if (startCircle === null ||
+                    endCircle === null ||
+                    startCircle.type !== endCircle.type) {
+                    if (end - start >= numMatches) {
+                        const result = [];
+                        for (let i = start; i < end; i++) {
+                            result.push(grid[x+i][grid[0].length-1-i]);
+                        }
+                        return result as Circle[];
+                    }
+                    start = end;
+                }
+            }
+            if (end - start >= numMatches) {
+                const result = [];
+                for (let i = start; i < end; i++) {
+                    result.push(grid[x+i][grid[0].length-1-i]);
+                }
+                return result as Circle[];
+            }
+        }
+
+        return null;
+    }
 }
