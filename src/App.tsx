90bf99b274b5edeb639d0ec777330db516e09380
Ferose B <ferose2@gmail.com>
2019-12-19 19:06:46 -0800

    Multipage working

--- src/App.tsx
+++ src/App.tsx
@@ -1,59 +1,53 @@
-import React from 'react';
+import * as React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.scss';
-import GameCanvas from './GameCanvas';
-import Banner from './Banner';
+
 import Menu from './Menu';
 import Button from 'react-bootstrap/Button';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faBars } from '@fortawesome/free-solid-svg-icons'
-import ConnectionPrompt from './ConnectionPrompt';
-import { UserManager, UserStateType, IUserListener } from './model/UserManager';
+import Banner from './Banner';
+import FourPlay from './FourPlay';
 
-interface IAppProps {}
-interface IAppState {
-    modalShow: boolean;
-    message: JSX.Element;
+interface IAppProps {
 }
 
-export default class App extends React.Component<IAppProps, IAppState> implements IUserListener {
-    private userManager: UserManager = new UserManager();
+interface IAppState {
+    modalShow: boolean;
+}
 
+export default class App extends React.Component<IAppProps, IAppState> {
     constructor(props: IAppProps) {
         super(props);
         this.state = {
-            message: <span></span>,
             modalShow: false,
         }
-
-        this.userManager.addListener(this);
     }
 
     public render() {
-         return (
+        return (
+           <BrowserRouter>
             <div className="App">
-            <Banner/>
-            <div id="alert">
-                {this.state.message}
+                <Banner/>
+                <Switch>
+                 <Route path="/" component={FourPlay} exact/>
+                 <Route path="/4play" component={FourPlay}/>
+               </Switch>
+
+               <Button
+                    variant="primary"
+                    id="menu-button"
+                    onClick={() => this.setState({modalShow: true})}
+                >
+                    <FontAwesomeIcon icon={faBars} />
+                </Button>
+
+                <Menu
+                    show={this.state.modalShow}
+                    onHide={() => this.setState({modalShow: false})}
+                />
             </div>
-            <GameCanvas userManager={this.userManager} setMessage={message => this.setState({message})}/>
-
-            {this.userManager.getUserState() !== UserStateType.Connected &&
-                <ConnectionPrompt userManager={this.userManager}/>
-            }
-
-            <Button
-                variant="primary"
-                id="menu-button"
-                onClick={() => this.setState({modalShow: true})}
-            >
-                <FontAwesomeIcon icon={faBars} />
-            </Button>
-
-            <Menu
-                show={this.state.modalShow}
-                onHide={() => this.setState({modalShow: false})}
-            />
-            </div>
-      );
+          </BrowserRouter>
+        );
     }
 }
--- template/Skeleton.tsx
+++ template/Skeleton.tsx
@@ -4,7 +4,7 @@ import './Skeleton.scss';
 interface ISkeletonProps {}
 interface ISkeletonState {}
 
-export default class Skeleton extends React.Component<ISkeletonProps, ISkeletonState> {
+export class Skeleton extends React.Component<ISkeletonProps, ISkeletonState> {
     public render() {
         return (
             <div className="Skeleton"></div>
